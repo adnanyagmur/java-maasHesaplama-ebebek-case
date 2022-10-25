ad6ef210d7a361ddc9ae658e338de22241802759
Adnan Yagmur <80180114+adnanyagmur@users.noreply.github.com>
2022-10-25 16:42:14 +0300

    FirstCommit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Employee employee = new Employee("Adnan",8000,45,1900);
+
+        employee.tax();
+        employee.bonus();
+        employee.raiseSalary();
+        employee.string();
+
+    }
+}
